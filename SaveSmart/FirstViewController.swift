6e2d8312d92fc3144990b83dada1365dab603492
brianli <brnli7@gmail.com>
2019-04-13 12:24:56 -0400

    Initial Commit

---
+++ SaveSmart/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  SaveSmart
+//
+//  Created by Brian Li on 4/13/19.
+//  Copyright © 2019 Brian Li. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
